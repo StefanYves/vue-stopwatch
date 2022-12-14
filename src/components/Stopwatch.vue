8671ec123ca90d6b3ff5f115f50baefdfbd87cad
StefanYves <stefannisipeanu02@gmail.com>
2022-12-14 16:17:13 +0200

    Vue-stopwatch

---
+++ src/components/Stopwatch.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>{{ minutes }}:{{ seconds }}</p>
+    <button @click="start">Start</button>
+    <button @click="stop">Stop</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      minutes: 0,
+      seconds: 0,
+      interval: null,
+    };
+  },
+  methods: {
+    start() {
+      if (this.interval) return;
+      this.interval = setInterval(() => {
+        this.seconds++;
+        if (this.seconds >= 60) {
+          this.seconds = 0;
+          this.minutes++;
+        }
+      }, 1000);
+    },
+    stop() {
+      clearInterval(this.interval);
+      this.interval = null;
+    },
+    reset() {
+      this.minutes = 0;
+      this.seconds = 0;
+    },
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import Stopwatch from "./components/Stopwatch.vue";
+
+import "./assets/main.css";
+
+createApp(App).mount("#app");
